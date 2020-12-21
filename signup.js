82fa829d93067990a26f671e4771d86adf00d293
Aditya Abhishek <encarcio95@gmail.com>
2020-12-22 00:05:10 +0530

    minor-fix login and signup missing value extraction

--- signup.js
+++ signup.js
@@ -3,10 +3,15 @@ function hoverNavLink(id) {
     elem.classList.add("text-primary");
 }
 function matchPassword(ID1,ID2) {
-    var password=document.getElementById(ID1);
-    var confirmPassword=document.getElementById(ID2);
-    if(!(password===confirmPassword))
+    var password=document.getElementById(ID1).value;
+    var confirmPassword=document.getElementById(ID2).value;
+    console.log(password);
+    console.log(confirmPassword);
+    if(password===confirmPassword)
     {
+       return; 
+    }
+    else{
         alert("Please re-enter password");
     }
 }
