88ee4cc5b126a50e8eeda6277c1bf9ec3124c5c2
Aditya Abhishek <encarcio95@gmail.com>
2021-04-17 17:36:58 +0530

    indexRoute call changed

--- signup.js
+++ signup.js
@@ -22,10 +22,11 @@
 
         var xhr = new XMLHttpRequest();
         xhr.withCredentials = false;
-
+        
         xhr.addEventListener("readystatechange", function() {
         if(this.readyState === 4) {
             console.log(this.responseText);
+            indexRoute();
         }
         });
 
